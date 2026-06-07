f638f1b25a34c63a70a6cfcffd9c698fab84778d
Beshwanthsai <2300030312@kluniversity.in>
2026-06-07 23:55:07 +0530

    by using getmapping and postmapping we can create and add the data we will use this  kept response status to 201 created

--- src/main/java/com/example/Crud_App/controller/TodoController.java
+++ src/main/java/com/example/Crud_App/controller/TodoController.java
@@ -3,8 +3,8 @@ package com.example.Crud_App.controller;
 import com.example.Crud_App.entity.Todo;
 import com.example.Crud_App.repo.TodoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +15,21 @@ public class TodoController {
 
     @GetMapping("/todos")
     public List<Todo> getAllTodos(){
-    List<Todo> todos = repo.findAll();
-    return todos;
+        List<Todo> todos = repo.findAll();
+        return todos;
     }
+
+    @GetMapping("/todos/{id}")
+    public Todo getTodo(@PathVariable Long id) {
+        return repo.findById(id.intValue()).get() ;
+    }
+
+    @PostMapping("/todos/add")
+    @ResponseStatus(code=HttpStatus.CREATED)
+    public Todo createTodo(@RequestBody Todo todos){
+        return repo.save(todos);
+    }
+
+//    @PutMapping("/todos/")
+//    public Todo UpdateTodo(@Reque)
 }
